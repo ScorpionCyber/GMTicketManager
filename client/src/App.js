bebaf0dfa4dae0b8ee36ae8c10a9cee6b45b57df
ScorpionCyber <moshko4work@gmail.com>
2020-08-24 11:33:29 +0300

    commiting

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+// import Ticket from './components/Ticket'
+import "./App.css";
+
+function App() {
+  return <main>aca</main>;
+}
+
+export default App;
